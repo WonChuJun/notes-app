0d5d75cc91275e7b9b3c87c19494a6ac6c3cca3a
Won Chu Jun <chujunwon2@gmail.com>
2026-02-26 23:59:50 +0800

    Make server use environment port for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 app.use(cors());
 app.use(express.json());
 const notesRoutes = require("./routes/notes");
